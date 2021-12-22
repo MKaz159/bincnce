c5d9b797fa12c2b0a44ea091bb3bcf505cdd911c
markk <markk2316@gmail.com>
2021-12-22 11:19:55 +0200

    Class Updated

--- main.py
+++ main.py
@@ -17,61 +17,52 @@ class Stock:
         self.ticker = ticker
         self.howlong = howlong
 
-    def get_axis_for_ohclv(self):
-        list_momentum = []
-        list_closing_times = []
+    def GetHistoricalData(self):
+        """
+        :param ticker:
+        :param howLong:
+        :return:
+        list of list containing stock details (prices)
+        """
         howLong = self.howlong
-        ticker = self.ticker
         # Calculate the timestamps for the binance api function
         untilThisDate = datetime.now()
         print(untilThisDate)
         sinceThisDate = untilThisDate - timedelta(days=howLong)
         print(sinceThisDate)
         # Execute the query from binance - timestamps must be converted to strings !
-        candle = client.get_historical_klines(str(ticker), Client.KLINE_INTERVAL_1WEEK, str(sinceThisDate),
+        candle = client.get_historical_klines(str(self.ticker), Client.KLINE_INTERVAL_1WEEK, str(sinceThisDate),
                                               str(untilThisDate))
-        i = 0
-        closing_price = float([i][closing_price_index])
-        closing_price_weeklater = float([i + 1][closing_price_index])
+        return candle
+
+    def GetMomentum(self,candle, week_number):
+        closing_price = float(candle[week_number][closing_price_index])
+        closing_price_weeklater = float(candle[week_number + 1][closing_price_index])
         momentum = closing_price - closing_price_weeklater
-        time_stamp = [i + 1][closing_time_index]
+        time_stamp = candle[week_number + 1][closing_time_index]
         time_stamp = str((datetime.fromtimestamp(int(time_stamp / 1000))))
         time_stamp = time_stamp.split()
-        date = time_stamp[0]
-        for length_of_weeks in range(len(candle) - 1):
+        return momentum, time_stamp[0]
 
 
-def GetStockPricing(candle_data):
-    """
-    :return:
-    A list containing stock closing prices in weekly candles
-    throughout the year
-    """
-    stock_prices = []
-    for week in candle_data:
-        stock_prices.append(week[closing_price_index])
-    return stock_prices
-
-
-def Get_axis_graph(candle_data):
+def Get_axis_graph(CoinStock, candle):
     list_momentum = []
     list_closing_times = []
     i = 0
-    for length_of_weeks in range(len(candle_data) - 1):
-        momentum, close_time = GetMomentum(candle_data, i)
+    for week in range(len(candle) - 1):
+        momentum, close_time = CoinStock.GetMomentum(candle,i)
         list_momentum.append(momentum)
         list_closing_times.append(close_time)
         i += 1
-    x = list_momentum
-    y = list_closing_times
+    x = list_closing_times
+    y = list_momentum
     return x, y
 
 
 def main():
-    ticker = 'BTCUSDT'
-    candle_data = GetHistoricalData(365, ticker)
-    x, y = Get_axis_graph(candle_data)
-    stock_prices = GetStockPricing(candle_data)
+    BTC = Stock('BTCUSDT', 365)
+    candle_db = BTC.GetHistoricalData()
+    x, y = Get_axis_graph(BTC, candle_db)
 
 
 if __name__ == '__main__':
