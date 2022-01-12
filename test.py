0cf45d6c6f640373d542794989c68b906c901610
markk <markk2316@gmail.com>
2022-01-12 15:33:39 +0200

    changes for class

--- test.py
+++ test.py
@@ -31,4 +31,6 @@ for key, value in coin_to_candle_DB.items():
     momentum_value, momentum_time = GetMomentum(value, len(value)-1)
     print(f' {key} momentum is {momentum_value} at {momentum_time}')
     stocks_momentum.update({key: momentum_value})
-# def Printing_Best_momentum(coins):
+max_key = max(stocks_momentum, key=stocks_momentum.get)
+print(f'The highest momentum stock is {max_key} with a value of {stocks_momentum[max_key]}')
+
